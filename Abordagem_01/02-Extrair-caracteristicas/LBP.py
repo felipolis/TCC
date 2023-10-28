c7a23c293aa8d49be1c67be54f1126eb0746ba63
Felipe Archanjo <felipecunhamendes@gmail.com>
2023-10-28 01:06:37 -0300

    Adicionando novas abordagens

---
+++ Abordagem_01/02-Extrair-caracteristicas/LBP.py
@@ -0,0 +1,85 @@
+import os
+import cv2
+import pandas as pd
+from skimage import feature
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Diretórios
+spectrogram_dir = "../../data/espectrogramas"
+diretorio_caracteristicas = '../../data/caracteristicas/'
+arquivo_csv = '../../data/caracteristicas/LBP.csv'
+
+# Parâmetros LBP
+QUANT_CLASSES = 10
+POINTS = 8
+RADIUS = 2
+
+# Encontre as classes mais comuns
+class_names = []
+class_count = {}
+
+for root, dirs, files in os.walk(spectrogram_dir):
+    for dir in dirs:
+        class_names.append(dir)
+
+for root, dirs, files in os.walk(spectrogram_dir):
+    for dir in dirs:
+        class_count[dir] = len(os.listdir(os.path.join(root, dir)))
+
+class_count = dict(sorted(class_count.items(), key=lambda item: item[1], reverse=True))
+classes = list(class_count.keys())[:QUANT_CLASSES]
+print(f"Classes com mais imagens: {classes}")
+
+# Crie o diretório se não existir
+if not os.path.exists(diretorio_caracteristicas):
+    os.makedirs(diretorio_caracteristicas)
+
+def calcular_LBP(imagem):
+    # Calcula o vetor de características LBP
+    lbp = feature.local_binary_pattern(imagem, POINTS, RADIUS, method="default")
+    n_bins = int(lbp.max() + 1)
+    hist, _ = np.histogram(lbp.ravel(), bins=n_bins, range=(0, n_bins))
+
+    # Normaliza o histograma
+    hist = hist.astype("float")
+    hist /= (hist.sum() + 1e-7)
+
+    return hist
+
+# Inicialize um DataFrame vazio para armazenar as características LBP
+df = pd.DataFrame()
+
+# Percorra os diretórios da lista de classes
+for key in os.listdir(spectrogram_dir):
+    if key in classes:
+        key_path = os.path.join(spectrogram_dir, key)
+
+        # Percorra todas as imagens no diretório
+        for image_name in os.listdir(key_path):
+            image_path = os.path.join(key_path, image_name)
+
+            # Carregue a imagem
+            image = cv2.imread(image_path)
+
+            # Converta para escala de cinza
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+            # Calcule o vetor de características LBP
+            caracteristicas_lbp = calcular_LBP(image)
+
+            # Obtenha o nome do arquivo original (tira o .png)
+            nome_arquivo_original = image_name.split('.')[0]
+            
+            # Adicione as características e o nome do arquivo ao DataFrame
+            data_to_append = pd.Series([key, nome_arquivo_original] + list(caracteristicas_lbp))
+            df = pd.concat([df, data_to_append.to_frame().T], ignore_index=True)
+
+            print(f'Processando {image_path}')
+
+# Defina os nomes das colunas no DataFrame
+colunas = ['key', 'nome_arquivo'] + [f'LBP_{i}' for i in range(len(caracteristicas_lbp))]
+df.columns = colunas
+
+# Salve o DataFrame em um arquivo CSV
+df.to_csv(arquivo_csv, index=False)
